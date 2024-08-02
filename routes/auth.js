da0868bfd6a9646563a9f32b244e9085d0c0fa29
surbhigoel555 <surbhigoel555@gmail.com>
2024-08-02 17:06:40 +0530

    Bug fixes and authenticationa added

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "any secret string";
 
 // Create User : POST "/api/auth/signup"
 // It doesn't require authentication
@@ -14,27 +17,49 @@ router.post(
     body("email", "Enter Valid email-id").isEmail(),
     body("password", "Minimum length of password must be 5").isLength({ min: 5 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) =>
-        res.send({
-          status: "OK",
-          msg: "User created successfully",
+    try {
+      //Check unique email-id
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ error: "Email already exists" });
+      }
+
+      const salt = await bcrypt.genSaltSync(10);
+      const pass = await bcrypt.hashSync(req.body.password, salt);
+
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: pass,
+      })
+        .then((user) => {
+          const data = {
+            user: { id: user.id },
+          };
+
+          // var token = jwt.sign(data, jwt_secret);
+          const token = jwt.sign(data, JWT_SECRET);
+
+          res.send({
+            status: "OK",
+            token: token,
+            msg: "User created successfully",
+          });
         })
-      )
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json({ error: err.errmsg });
-      });
+        .catch((err) => {
+          console.log(err);
+          res.status(400).json({ error: err.errmsg });
+        });
+    } catch (error) {
+      console.error("error ", error);
+      return res.status(500).json("Internal server error");
+    }
   }
 );
 
