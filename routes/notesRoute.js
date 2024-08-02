8c5ec8b3bbd0c2f04e53fac40a74cc53ccb330e7
surbhigoel555 <surbhigoel555@gmail.com>
2024-08-02 19:50:23 +0530

    update-note API added

--- routes/notesRoute.js
+++ routes/notesRoute.js
@@ -55,4 +55,50 @@ router.post(
   }
 );
 
+// update-note - Update Existing Note
+// - /api/note/update-note
+router.put(
+  "/update-note/:id",
+  fetchUserDetails,
+  [
+    body("title", "Enter Valid Title").isLength({ min: 3 }),
+    body("description", "Enter Valid description").isLength({ min: 1 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const note = await Notes.findById(req.params.id);
+      // If note doesn't exist
+      if (!note) {
+        return res.status(404).json({ error: "Not Found" });
+      }
+
+      // If author doesn't match
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).json({ error: "Unauthorized" });
+      }
+
+      const newNote = {
+        title: req.body.title,
+        description: req.body.description,
+      };
+
+      //{new: true}: It there is new content, It'll create new entry
+      await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+      res.send({
+        status: "OK",
+        msg: "Note updated successfully",
+      });
+    } catch (error) {
+      console.error("error ", error);
+      return res.status(500).json("Internal server error");
+    }
+  }
+);
+
 module.exports = router;
