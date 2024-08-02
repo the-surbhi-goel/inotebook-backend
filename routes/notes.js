47a1e4c9297c534ee670a5d29ba5e94efce832f7
surbhigoel555 <surbhigoel555@gmail.com>
2024-08-02 19:02:20 +0530

    get-notes-list API added

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchUserDetails = require("../middleware/fetchUserDetails");
 
-router.get("/", (req, res) => {
-  res.send("notes");
+// get-notes-list - Get All notes
+// Require authentication
+router.get("/get-notes-list", fetchUserDetails, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.send(notes);
 });
 
 module.exports = router;
