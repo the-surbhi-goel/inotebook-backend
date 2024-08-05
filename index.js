9b8515675f0c2e08af545717a0c5ab5dcea67ca5
surbhigoel555 <surbhigoel555@gmail.com>
2024-08-05 19:07:05 +0530

    CORS package added

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const connectToMongo = require("./db");
 
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 
 connectToMongo();
-const app = express()
+const app = express();
+app.use(cors());
 const port = 8000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.use("/api/auth", require('./routes/authRoute'))
-app.use("/api/user", require('./routes/userRoute'))
-app.use("/api/notes", require('./routes/notesRoute'))
+app.use("/api/auth", require("./routes/authRoute"));
+app.use("/api/user", require("./routes/userRoute"));
+app.use("/api/notes", require("./routes/notesRoute"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
